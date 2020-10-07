f2eccfa0e577934dbfec215198aa115b132bc28e
EC2 Default User <ec2-user@ip-172-16-130-58.ec2.internal>
2020-10-07 16:28:17 +0000

    add the reply function

--- line-bot-chap/lineBot/app.py
+++ line-bot-chap/lineBot/app.py
@@ -1,10 +1,23 @@
 import json, os, logging
 from RandomWordGenerator import RandomWord
-from request import post
+from requests import post
 
 rw = RandomWord(max_word_size = 6)
+rwgenerated = rw.generate()
+accessToken = os.environ['ACCESS TOKEN']
+
 def reply(accessToken, replyToken):
-    
+    url = 'https://api.line.me/v2/bot/message/reply'
+    headers = {Authorization: 'Bearer '+ accessToken}
+    data = {
+    "replyToken":replyToken,
+    "messages":[
+        {
+            "type":"text",
+            "text":"rwgenerated"
+        }
+    ]}
+    return post(url = url, headers = headers, data = data)
 
 def answer(event, context):
 
