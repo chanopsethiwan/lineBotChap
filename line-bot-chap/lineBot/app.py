a9f821e5297098b050184ed91d2bf92fd7b2b828
chap <chap@Apples-MacBook-Pro.local>
2020-10-09 16:17:44 +0700

    fixed app.py

--- line-bot-chap/lineBot/app.py
+++ line-bot-chap/lineBot/app.py
@@ -2,12 +2,12 @@ import json, os, logging
 from RandomWordGenerator import RandomWord
 from requests import post
 
-rw = RandomWord(max_word_size = 6)
-rwgenerated = rw.generate()
-rwgeneratedupper = rwgenerated.upper()
 accessToken = os.environ['ACCESS_TOKEN']
 
 def reply(accessToken, replyToken):
+    rw = RandomWord(max_word_size = 6)
+    rwgenerated = rw.generate()
+    rwgeneratedupper = rwgenerated.upper()
     url = 'https://api.line.me/v2/bot/message/reply'
     headers = {'Authorization': 'Bearer '+ accessToken}
     data = {
@@ -18,7 +18,7 @@ def reply(accessToken, replyToken):
             "text":rwgeneratedupper
         }
     ]}
-    return post(url = url, headers = headers, data = data)
+    return post(url = url, headers = headers, json = data)
 
 def answer(event, context):
     lineWebhookObject = json.loads(event['body'])
