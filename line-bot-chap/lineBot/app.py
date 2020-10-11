b23b137994f18ff1d1240f6f9491455b89940208
chap <chanopsethiwan233@gmail.com>
2020-10-12 00:13:23 +0700

    added chatterbot

--- line-bot-chap/lineBot/app.py
+++ line-bot-chap/lineBot/app.py
@@ -1,13 +1,28 @@
 import json, os, logging
 from RandomWordGenerator import RandomWord
 from requests import post
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 accessToken = os.environ['ACCESS_TOKEN']
+chatbot = ChatBot("lineBotChap")
+conversation = [
+    "Hi",
+    "Hello",
+    "How are you doing?",
+    "I'm doing great",
+    "Thank you",
+    "You're welcome"
+]
 
-def reply(accessToken, replyToken):
-    rw = RandomWord(max_word_size = 6)
-    rwgenerated = rw.generate()
-    rwgeneratedupper = rwgenerated.upper()
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+
+def reply(accessToken, replyToken, text):
+#     rw = RandomWord(max_word_size = 6)
+#     rwgenerated = rw.generate()
+#     rwgeneratedupper = rwgenerated.upper()
+    response = chatbot.get_response(text)
     url = 'https://api.line.me/v2/bot/message/reply'
     headers = {'Authorization': 'Bearer '+ accessToken}
     data = {
@@ -15,15 +30,16 @@ def reply(accessToken, replyToken):
     "messages":[
         {
             "type":"text",
-            "text":rwgeneratedupper
+            "text":response
         }
     ]}
     return post(url = url, headers = headers, json = data)
 
 def answer(event, context):
-    lineWebhookObject = json.loads(event['body'])
+    lineWebhookObject = json.loads(event['body']) 
     replyToken = lineWebhookObject['events'][0]['replyToken']
-    message = reply(accessToken, replyToken)
+    text_by_user = lineWebhookObject['events'][0]['message']['text']
+    message = reply(accessToken, replyToken, text_by_user)
     return {
         "statusCode": 200,
         "body": json.dumps({
