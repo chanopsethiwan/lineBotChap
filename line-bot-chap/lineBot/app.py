2dd702cd89b985ba52fef715a6694a8b39cb1a7d
chap <chanopsethiwan233@gmail.com>
2020-11-06 22:13:25 +0700

    added a new layer

--- line-bot-chap/lineBot/app.py
+++ line-bot-chap/lineBot/app.py
@@ -6,12 +6,22 @@ import pickle
 from s3bz.s3bz import S3
 
 accessToken = os.environ['ACCESS_TOKEN']
-pickleChatbotDict = S3.load(
-key = "chatBotTrained",
+# pickleChatbotDict = S3.load(
+# key = "chatBotTrained",
+# bucket = 'trained-bot'
+# )
+# pickleChatbot = pickleChatbotDict["pickleChatbot"]
+# chatbot = pickle.loads(pickleChatBot)
+chatbot = ChatBot("lineBotChap", storage_adapter ='chatterbot.storage.SQLStorageAdapter', proprecessors = ['chatterbot.preprocessors.clean_whitespace'], database_uri = 'sqlite:////tmp/databse.db', read_only=True)
+
+# with open(credLocation , 'rb') as f:
+#     creden = pickle.load(f)
+#     PW = creden['pw']
+#     USER = creden['user']
+key = 'chatterbotdb.db'
+path = '/tmp/databse.db'
 bucket = 'trained-bot'
-)
-pickleChatbot = pickleChatbotDict['pickleChatbot']
-chatbot = pickle.loads(pickleChatBot)
+S3.loadFile(key = key, path = path, bucket = bucket)
 
 def reply(accessToken, replyToken, text):
     response = chatbot.get_response(text)
--- line-bot-chap/lineBot/training.py
+++ line-bot-chap/lineBot/training.py
@@ -3,7 +3,7 @@ from s3bz.s3bz import S3
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-chatbot = ChatBot("lineBotChap")
+chatbot = ChatBot("lineBotChap", storage_adapter ='chatterbot.storage.SQLStorageAdapter', proprecessors = ['chatterbot.preprocessors.clean_whitespace'], database_uri = 'sqlite:////tmp/databse.db', read_only=True)
 conversation = [
     "Hi",
     "Hello",
@@ -16,10 +16,15 @@ conversation = [
 trainer = ListTrainer(chatbot)
 trainer.train(conversation)
 
-pickleChatbot = pickle.dumps(chatbot)
-pickleChatbotDict = {"pickleChatbot": pickleChatbot}
-S3.save(
-key = 'chatBotTrained',
-objectToSave = pickleChatbotDict,
+# pickleChatbot = pickle.dumps(chatbot)
+# pickleChatbotDict = {"pickleChatbot": pickleChatbot}
+# S3.save(
+# key = 'chatBotTrained',
+# objectToSave = pickleChatbotDict,
+# bucket = 'trained-bot'
+# )
+
+key = 'chatterbotdb.db'
+path = '/tmp/databse.db'
 bucket = 'trained-bot'
-)
+S3.saveFile(key = key, path = path, bucket = bucket)
